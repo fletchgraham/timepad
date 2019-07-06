c610926d696675f9d50ef36b7787b2f2ca58f9cd
Fletcher Graham <sfletchgraham@gmail.com>
2019-07-05 23:48:58 -0400

    got the timeline working with real epoch values.

--- timepad/static/sketch.js
+++ timepad/static/sketch.js
@@ -1,44 +1,37 @@
 
-///// Global Variales /////
+//////////////////////////////////////////////////////////////////////////////
+// Global Variables
 
+// view transforms
 var OFFSET = 0;
+var ZOOM = 1;
+
+// for calculating drag deltas
 var DOWN_Y = 0;
 var DELTA_Y = 0;
 
-let FILE = 'Try dropping a file anywhere.';
+let FILE = '';
 let FRAMES = [];
 
-///// Setup and Draw /////
+//////////////////////////////////////////////////////////////////////////////
+// Setup and Draww
 
 function setup() {
+  OFFSET = 0;
   const c = createCanvas(windowWidth,windowHeight);
   background(24);
-  c.drop(gotFile);
-  textSize(20)
+  c.drop(gotFile); // dropfile event triggers callback
 }
 
 function draw() {
   background(24);
-
   drawTimeline();
-
-  // test elipse
-  noFill();
-  stroke(255);
-  ellipse(width/2, height/2 + OFFSET, 200);
-
-  // test dropfile text
-  fill(255);
-  noStroke();
-  text(FILE, 50, 100)
-
-  
-
-  // don't redraw unless something happens
-  noLoop();
+  drawDebug();
+  //noLoop();
 }
 
-///// Events /////
+//////////////////////////////////////////////////////////////////////////////
+// Events
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -54,28 +47,67 @@ function touchStarted() {
 
 function touchMoved() {
   DELTA_Y = mouseY - DOWN_Y;
-  OFFSET = START + DELTA_Y;
+  OFFSET = START + DELTA_Y * ZOOM;
   redraw();
   return false;
 }
 
 function gotFile(file) {
-  FILE = 'Got that file :)';
+  FILE = 'You dropped a file! Good Job!';
   redraw();
 }
 
-///// Models /////
+//////////////////////////////////////////////////////////////////////////////
+// Models
 
 // frame array: [start, stop, project, uid, tags, edited?]
 
 
+//////////////////////////////////////////////////////////////////////////////
+// Misc
+
+function now() {
+  n = new Date() / 1000;
+  return n;
+}
+
+function toSeconds(pixels) {
+  secs = Math.round(-(pixels - height/2) * ZOOM + now() + OFFSET);
+  return secs;
+}
+
+function toPixels(seconds) {
+  px = -((seconds - now() -  OFFSET) / ZOOM) + height/2;
+  return px;
+}
+
 function drawTimeline() {
   for (var i = 0; i < height; i++) {
-    if ((i - OFFSET) % 100 == 0) {
+    if ((toSeconds(i)) % 100 == 0) {
       stroke(50);
-      line(0, i, width, i)
+      fill(50);
+      line(0, i, width, i);
+      textSize(16);
+      text(str(toSeconds(i)), 50, i);
     }
   }
 }
 
+function drawDebug() {
+  // font settings
+  fill(255);
+  textSize(20);
+  noStroke();
+  textAlign(RIGHT);
+  margin = 50;
+
+  // test dropfile text
+  text(FILE, width - margin, height - margin)
+  text('offset: ' + str(OFFSET), width - margin, height - margin - 25);
+
+  // draw now marker
+  text('<<<<<<< NOW >>>>>>>', width - margin, toPixels(now()));
+
+  textAlign(LEFT);
+}
 
