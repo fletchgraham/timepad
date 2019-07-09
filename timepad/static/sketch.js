52046abcb8ee48742f53aac7852b42ad444d6fa7
Fletcher Graham <sfletchgraham@gmail.com>
2019-07-09 00:46:31 -0400

    refactor a lot. change the way frames are started.

--- timepad/static/sketch.js
+++ timepad/static/sketch.js
@@ -3,71 +3,97 @@
 // Global Variables
 
 // view transforms
+var LAST_OFFSET = 0;
 var OFFSET = 0;
 var ZOOM = 1;
-var START = 0;
 
 // for calculating drag deltas
 var DOWN_Y = 0;
 var DELTA_Y = 0;
+var START = 0;
 
+// dropfile indicator
 let FILE = '';
+
+// model
 let FRAMES = [];
 
-// modes
-var MODE = 'PAN';
-
-let add_frame_button;
-let delete_frame_button;
-
-let BUTTON_PRESSED = false;
+// ui
+var CONTEXT;
+let start_btn;
+let delete_btn;
+let timeline;
 
 
 //////////////////////////////////////////////////////////////////////////////
 // Setup and Draww
 
 function setup() {
+  frameRate(1);
   OFFSET = now();
-  background(24);
+  LAST_OFFSET = 0;
+  CONTEXT = 'SCRUB';
   const c = createCanvas(windowWidth,windowHeight);
-
-  // delet frame button
-  delete_frame_button = createButton('Delete Frame');
-  delete_frame_button.style('color', 'red')
-  delete_frame_button.position(10, 45);
-  delete_frame_button.touchStarted(deleteFrame);
-  delete_frame_button.mousePressed(deleteFrame);
-  delete_frame_button.addClass('button')
-  
-  // add frame button
-  add_frame_button = createButton('Add Frame');
-  add_frame_button.position(10, 100);
-  add_frame_button.touchStarted(createFrame);
-  add_frame_button.mousePressed(createFrame);
-  add_frame_button.addClass('button')
-
   c.drop(gotFile); // dropfile event triggers callback
 
-  // init ui elements
+  // delet frame button
+  delete_btn = createButton('Delete Frame');
+  delete_btn.style('color', 'red')
+  delete_btn.position(10, 45);
+  delete_btn.mousePressed(delete_btn_callback);
+  delete_btn.touchStarted(delete_btn_callback);
+  delete_btn.addClass('button')
+  
+  // add frame button
+  start_btn = createButton('Start / Stop');
+  start_btn.position(10, 100);
+  start_btn.mousePressed(start_btn_callback);
+  start_btn.addClass('button')
+
   timeline = new Timeline();
 
-  RES = 'Response';
   loadFrames();
 }
 
 function draw() {
+  OFFSET = LAST_OFFSET + now();
   background(24);
   timeline.render();
   for (f in FRAMES) {
-    FRAMES[f].render();
+    frame = FRAMES[f];
+    if (frame.recording == true) {
+      frame.stop = OFFSET;
+    }
+    renderFrame(frame);
   }
-  drawCrosshair();
-  drawDebug();
-  noLoop();
+  renderCrosshair();
+  renderDebug();
+  //noLoop();
 }
 
 //////////////////////////////////////////////////////////////////////////////
-// Events
+// Button Callbacks
+
+function start_btn_callback() {
+  CONTEXT = 'BTN_PRESSED';
+  startFrame();
+  start_btn.mousePressed(stop_btn_callback);
+}
+
+function stop_btn_callback() {
+  CONTEXT = 'BTN_PRESSED';
+  stopFrame();
+  start_btn.mousePressed(start_btn_callback);
+}
+
+
+function delete_btn_callback() {
+  CONTEXT = 'BTN_PRESSED';
+  deleteFrame();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// p5 Events
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -75,8 +101,8 @@ function windowResized() {
 }
 
 function touchStarted() {
-  if (BUTTON_PRESSED == true) {
-    BUTTON_PRESSED = false;
+  if (CONTEXT == 'BTN_PRESSED') {
+    return;
   } else {
     framesTouch();
     timeline.touched();
@@ -84,10 +110,18 @@ function touchStarted() {
   }
 }
 
+function touchEnded() {
+  CONTEXT = 'SCRUB';
+}
+
 function touchMoved() {
-  timeline.dragged();
-  redraw();
-  return false;
+  if (CONTEXT == 'BTN_PRESSED') {
+    return false;
+  } else {
+    timeline.dragged();
+    redraw();
+    return false;
+  }
 }
 
 function gotFile(file) {
@@ -101,22 +135,15 @@ function gotFile(file) {
 class Timeline {
 
   touched() {
-    if (MODE != 'PAN') {
-      return false;
-    }
     START = OFFSET;
     DELTA_Y = mouseY;
     DOWN_Y = mouseY;
-    return true;
   }
 
   dragged() {
-    if (MODE != 'PAN') {
-      return false;
-    }
     DELTA_Y = mouseY - DOWN_Y;
     OFFSET = START + DELTA_Y * ZOOM;
-    return true;
+    LAST_OFFSET = OFFSET - now();
   }
 
   render() {
@@ -133,7 +160,7 @@ class Timeline {
 }
 
 //////////////////////////////////////////////////////////////////////////////
-// Frame
+// Frame Controls
 
 // frame array: [start, stop, project, uid, tags, edited?]
 
@@ -143,64 +170,81 @@ class Frame {
     this.stop = start + 100;
     this.project = 'project';
     this.selected = true;
-  }
-
-  over() {
-    var x = mouseX;
-    var y = mouseY;
-    var x1 = 50;
-    var x2 = width - 50;
-    var y1 = toPixels(this.stop);
-    var y2 = toPixels(this.start);
-
-    if (x1 < x && x < x2 && y1 < y && y < y2) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  render() {
-    // frame rect
-    rectMode(CORNERS);
-    stroke(0, 0 , 255);
-    if (this.selected == true) {
-      stroke(255);
-    }
-    fill(0,0,100);
-    rect(50, toPixels(this.stop), width - 50, toPixels(this.start), 10);
-
-    // frame info
-    noStroke();
-    fill(255);
-    text(this.project, 60, toPixels(this.start)-10)
+    this.recording = false;
   }
 }
 
+/** Returns true if the mouse is over the given frame. */
+function overFrame(frame) {
+  var x = mouseX;
+  var y = mouseY;
+  var x1 = 50;
+  var x2 = width - 50;
+  var y1 = toPixels(frame.stop);
+  var y2 = toPixels(frame.start);
+
+  if (x1 < x && x < x2 && y1 < y && y < y2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/** Draw the a representation of the given frame. */
+function renderFrame(frame) {
+  // frame rect
+  rectMode(CORNERS);
+  stroke(0, 0 , 255)
+  if (frame.selected == true) {
+    stroke(255);
+  }
+  fill(0,0,100);
+  if (frame.recording == true) {
+    noFill();
+  }
+  rect(50, toPixels(frame.stop), width - 50, toPixels(frame.start), 10);
+
+  // frame info
+  noStroke();
+  fill(255);
+  text(frame.project, 60, toPixels(frame.start)-10)
+}
+
+/** Loop through frames and remove those that are selected. */
 function deleteFrame() {
-  BUTTON_PRESSED = true;
   for (f in FRAMES) {
     if (FRAMES[f].selected == true) {
       FRAMES.splice(f, 1);
     }
   }
-  httpPost('/data/frames', JSON.stringify(FRAMES), function(result) {
-    redraw();
-  });
+  sync();
 }
 
-function createFrame() {
-  BUTTON_PRESSED = true;
+function startFrame() {
+  deselectFrames();
+  new_frame = new Frame(toSeconds(height / 2));
+  new_frame.recording = true;
+  FRAMES.push(new_frame);
+}
+
+function stopFrame() {
+  deselectFrames();
+  for (f in FRAMES) {
+    frame = FRAMES[f];
+    if (frame.recording == true) {
+      frame.recording = false;
+    }
+  }
+  sync();
+}
+
+function deselectFrames() {
   for (f in FRAMES) {
     FRAMES[f].selected = false;
   }
-  new_frame = new Frame(toSeconds(height / 2));
-  FRAMES.push(new_frame);
-  httpPost('/data/frames', JSON.stringify(FRAMES), function(result) {
-    redraw();
-  });
 }
 
+/** Leave only the frame that's directly under the cursor selected. */
 function framesTouch() {
   for (f in FRAMES) {
     FRAMES[f].selected = false;
@@ -208,7 +252,7 @@ function framesTouch() {
   // reverse loop so the top ones are seleced first
   for (var i = FRAMES.length; i--;) {
     var frame = FRAMES[i];
-    if (frame.over()) {
+    if (overFrame(frame)) {
       frame.selected = true;
       return true;
     }
@@ -234,8 +278,17 @@ function loadFrames() {
 //////////////////////////////////////////////////////////////////////////////
 // Helpful Utilities
 
+function sync() {
+  var response;
+  httpPost('/data/frames', JSON.stringify(FRAMES), function(result) {
+    response = result;
+    redraw();
+  });
+  return response;
+}
+
 function now() {
-  n = new Date() / 1000;
+  n = Math.round(new Date() / 1000);
   return n;
 }
 
@@ -252,7 +305,7 @@ function toPixels(seconds) {
 //////////////////////////////////////////////////////////////////////////////
 // Debug Info
 
-function drawCrosshair() {
+function renderCrosshair() {
   var size = 15;
   stroke(255);
   var mid = height/2;
@@ -261,7 +314,7 @@ function drawCrosshair() {
   line(mid, height/2 - size, mid, height/2 + size);
 }
 
-function drawDebug() {
+function renderDebug() {
   // font settings
   fill(255);
   textSize(16);
@@ -269,12 +322,13 @@ function drawDebug() {
   textAlign(RIGHT);
   margin = 10;
 
-  // test dropfile text
-  text(FILE, width - margin, height - margin - 25);
+  // debug redouts
+  text(FILE, width - margin, height - margin - 50);
+  text('context: ' + CONTEXT, width - margin, height - margin - 25);
   text('offset: ' + str(OFFSET), width - margin, height - margin);
 
   // draw now marker
-  text('<< NOW', width - margin, toPixels(now()));
+  text('NOW', width - margin, toPixels(now()));
 
   textAlign(LEFT);
 
