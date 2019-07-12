05e64ab31269b74f6c2425430266946b8763aa58
Fletcher Graham <sfletchgraham@gmail.com>
2019-07-11 21:29:19 -0400

    move utilities to their own js file.

--- timepad/static/sketch.js
+++ timepad/static/sketch.js
@@ -274,34 +274,6 @@ function loadFrames() {
   });
 }
 
-
-//////////////////////////////////////////////////////////////////////////////
-// Helpful Utilities
-
-function sync() {
-  var response;
-  httpPost('/data/frames', JSON.stringify(FRAMES), function(result) {
-    response = result;
-    redraw();
-  });
-  return response;
-}
-
-function now() {
-  n = Math.round(new Date() / 1000);
-  return n;
-}
-
-function toSeconds(pixels) {
-  secs = Math.round(-(pixels - height/2) * ZOOM + OFFSET);
-  return secs;
-}
-
-function toPixels(seconds) {
-  px = -((seconds - OFFSET) / ZOOM) + height/2;
-  return px;
-}
-
 //////////////////////////////////////////////////////////////////////////////
 // Debug Info
 
