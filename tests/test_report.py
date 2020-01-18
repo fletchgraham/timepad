db2fd4ffeaad8797e55dee9419f63ccb95d16d97
fletchgraham <sfletchgraham@gmail.com>
2020-01-18 16:34:30 -0500

    hackishly make the report do something.

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,3 +1,5 @@
+import json
+
 def test_report_loggedout(client):
     res = client.get('report/overview', follow_redirects=True).data
     assert b'Sign In' in res
@@ -6,3 +8,14 @@ def test_report_loggedin(client, auth):
     auth.login()
     res = client.get('report/overview').data
     assert b'Report' in res
+
+def test_overview(client, auth):
+    auth.login()
+    post_data = [
+        {'project':'project1','start':200,'stop':300},
+        {'project':'project2','start':500,'stop':1000},
+        ]
+    post_data = json.dumps(post_data)
+    client.post('data/frames', data=post_data).data
+    res = client.get('report/overview').data
+    assert b'project1' in res
--- timepad/report.py
+++ timepad/report.py
@@ -1,4 +1,5 @@
 import functools
+import json
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -13,7 +14,16 @@ bp = Blueprint('report', __name__, url_prefix='/report')
 @bp.route('/overview', methods=('GET', 'POST'))
 @login_required
 def report():
+    user_id = session.get('user_id')
     if request.method == 'POST':
         return 'Report Post'
 
-    return 'Report Get'
+    db = get_db()
+    frames = db.execute('select * from timeline where author_id=?', (user_id,)).fetchone()['frames']
+    res = '<h1>Report Get</h1>\n'
+    for frame in json.loads(frames):
+        try:
+            res += f"<p>{frame.get('project')}: {frame.get('stop') - frame.get('start')}</p>"
+        except:
+            pass
+    return res
